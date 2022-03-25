5d51c5701b32dc2abeca5ed54d6f78eb47a6dfb4
teotwa <guiraha@gmail.com>
2022-03-25 10:13:45 +0100

    Adding file 01a_import_runoff_info.R, done in class on 2022-03-25 (yyyy-mm-dd), which impors and make some adjustments in its structure to the DB runoff_station

---
+++ code/01a_import_runoff_info.R
@@ -0,0 +1,20 @@
+#01a_import_runoff_info.R
+#2022-03-25
+
+library(data.table)
+
+list.files('./data/raw')
+
+runoff_stations <- fread('./data/raw/runoff_stations.csv')
+
+head(runoff_stations)
+str(runoff_stations)
+runoff_stations$station
+
+runoff_stations[, sname := factor(abbreviate(station))] #abbreviate: trim by default to 
+runoff_stations[, id := factor(id)] #factor converts the field ID in a categorial variable
+runoff_stations[, lat := round(lat, 3)]
+runoff_stations[, lon := round(lon, 3)]
+runoff_stations[, altitude := round(altitude, 0)]
+
+saveRDS(runoff_stations, './data/runoff_stations_raw.rds')
